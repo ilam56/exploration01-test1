1e32015985e65de7fdb6e67f770449114c85935d
ilam56 <ilam56@mail.missouri.edu>
2020-09-19 23:54:55 -0500

    1 file modified, 4 deleted

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
-import { FirstComponent } from '../first/first.component';
-import { SecondComponent } from '../second/second.component';
 import { CalculatorComponent} from '../calculator/calculator.component';
 import { SampleComponent } from '../sample/sample.component';
 
 const routes: Routes = [
-  { path: 'first-component', component: FirstComponent },
-  { path: 'second-component', component: SecondComponent },
   { path: 'calculator-component', component: CalculatorComponent},
   { path: 'sample-component', component: SampleComponent},
 ]; // sets up routes constant where you define your routes
